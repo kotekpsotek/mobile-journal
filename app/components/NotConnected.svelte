59df9683102104321d93eac46f631a9d0895f572
kotekpsotek <siematumichael@protonmail.com>
2023-10-08 21:24:06 +0200

    Layout and styles for 'Not connected' screen

--- app/components/NotConnected.svelte
+++ app/components/NotConnected.svelte
@@ -1,3 +1,22 @@
-<page>
-    
+<page class="page">
+    <actionBar title="❌  No Internet Connection" color="red" background="whitesmoke"/>
+    <flexboxLayout flexDirection="column" alignItems="center" justifyContent="center">
+        <stackLayout color="white">
+            <label class="title">You don't have internet connection</label>
+            <label class="description">You have to be connected regards to use our violet 💐 app</label>
+        </stackLayout>
+    </flexboxLayout>
 </page>
+
+<style>
+    @import "../app.css";
+
+    .title {
+        font-size: 22px;
+        font-weight: 600;
+    }
+
+    .description {
+        font-size: 15px;
+    }
+</style>

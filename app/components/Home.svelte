6c8c30f2c85e8459604cae308da99d61ba677edf
kotekpsotek <siematumichael@protonmail.com>
2023-10-08 22:13:36 +0200

    News source screen image buttons layout with styles and global styles for page

--- app/components/Home.svelte
+++ app/components/Home.svelte
@@ -38,10 +38,19 @@
 <page class="main">
     <actionBar title="{selected ? selected.getNewsTitle() : ""}"/>
     {#if !selected.select}
-        <flexboxLayout flexDirection="row" justifyContent="center" alignItems="center">
-            <button id="polsat" on:tap={selected.selectNew("polsat")}>POLSAT NEWS</button>
-            <button id="tvp" on:tap={selected.selectNew("tvp")}>TVP INFO</button>
-            <button id="tvn" on:tap={selected.selectNew("tvn")}>TVN 24</button>
+        <flexboxLayout class="layout" flexDirection="column" justifyContent="center" alignItems="center">
+            <stackLayout class="btn-img" data-polsat orientation="horizontal" padding="5" on:tap={selected.selectNew("polsat")} >]
+                <image class="img" src="~/images/polsat-news.png"/>
+                <label class="label" text="POLSAT NEWS"/>
+            </stackLayout>
+            <stackLayout class="btn-img" data-tvp orientation="horizontal" padding="5" on:tap={selected.selectNew("tvp")} >]
+                <image class="img" src="~/images/tvp-info-logo.png"/>
+                <label class="label" text="TVP INFO" color="black"/>
+            </stackLayout>
+            <stackLayout class="btn-img" data-tvn orientation="horizontal" padding="5" on:tap={selected.selectNew("tvn")} >]
+                <image class="img" src="~/images/tvn-24.png"/>
+                <label class="label" color="black">TVN 24</label>
+            </stackLayout>
         </flexboxLayout>
     {:else}
         <webView src="{selected.getNewsUrl()}"/>
@@ -50,6 +59,41 @@
 
 <style>
     .main {
-        background-color: black;
+        background-color: rgb(8, 112, 164);
+    }
+
+    .btn-img {
+        width: 75%;
+        height: 400px;
+        background-color: white;
+        margin-top: 45px;
+        border-radius: 50px;
+    }
+
+    [data-polsat] {
+        background-color: rgb(0, 0, 0);
+    }
+
+    [data-tvp] {
+        background-color: whitesmoke;
+    }
+
+    [data-tvn] {
+        background-color: white;
+    }
+
+    .img {
+        width: 55%;
+        height: 100%;
+        object-fit: fill;
+    }
+
+    .label {
+        width: 45%;
+        height: 100%;
+        text-align: center;
+        color: white;
+        font-size: 16px;
+        font-weight: 600;
     }
 </style>
